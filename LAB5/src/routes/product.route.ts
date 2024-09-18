37d2b7d0579c9ca69abdec14bb6ca473b39afbf0
natnarong326 <nxtnx09@gmail.com>
2024-09-18 13:48:29 +0700

    Add files via upload

---
+++ LAB5/src/routes/product.route.ts
@@ -0,0 +1,4 @@
+import { Router } from "express";
+import productController from "../controller/product.controller"; // Assuming the controller file is located in the parent directory of the current file
+
+const productRouter = Router();
